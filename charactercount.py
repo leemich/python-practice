bd114592d138e862a9ad39495b62908704c3bbaf
Mich Lee <inthedeep75@gmail.com>
2015-11-29 15:28:06 -0500

    Remove deleted files

--- charactercount.py
+++
@@ -1,8 +0,0 @@
-message= 'Hello.'
-count = {}
-
-for character in message:
-    count.setdefault(character, 0)
-    count[character] = count[character] + 1
-    charlist = list(count.keys())
-print(charlist, sep='/')
--- str_format.py
+++
@@ -1,5 +0,0 @@
-result = 'Hello, {}. You are {} years old.'
-name = 'Julie'
-age = '40'
-result = result.format(name, age)
-print(result)
